7871ba3d2c2f57a4d938b72c3130afb56aaa881b
Kshitij1K <kabeer@iitk.ac.in>
2020-06-07 00:07:22 +0530

    Add a link state publisher plugin
    This a model plugin that publishes the pose of the particular link that
    is specified in the element <link_pose>. Currently being used to
    publish the pose of the mast and communications block.

---
+++ include/iarc_simulation_tools/link_state_publisher.hpp
@@ -0,0 +1,26 @@
+#include <functional>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <ignition/math/Vector3.hh>
+
+namespace gazebo {
+
+class LinkStatePublisher : public ModelPlugin {
+    public:
+        void Load(physics::ModelPtr parent, sdf::ElementPtr sdf);
+        void onUpdate();
+
+    private:
+        physics::ModelPtr model_;
+        sdf::ElementPtr sdf_;
+        event::ConnectionPtr updateConnection_;
+        physics::LinkPtr link_;
+        std::string link_name_;
+        
+        transport::NodePtr node_;
+        transport::PublisherPtr pose_pub_;
+};
+
+GZ_REGISTER_MODEL_PLUGIN(LinkStatePublisher);
+}
