b9c63d0d57a5e85269fa74cc744d1f1dec9654f9
Kshitij1K <kabeer@iitk.ac.in>
2020-06-07 00:17:09 +0530

    Add pose subscriber to light_plugin
    The plugin now subscribes to the poses of mast and communication module
    which is published by link_state_publisher plugin. This will be used to
    dynamically switch on/off the light in response to the position of
    the communications module.

--- include/iarc_simulation_tools/light_plugin.hpp
+++ include/iarc_simulation_tools/light_plugin.hpp
@@ -4,6 +4,7 @@
 #include <gazebo/common/common.hh>
 #include <thread>
 #include <chrono>
+#include <eigen3/Eigen/Eigen>
 
 namespace gazebo {
 
@@ -11,11 +12,26 @@ class GZ_PLUGIN_VISIBLE ModelLight : public VisualPlugin{
     public:
         void Load(rendering::VisualPtr parent, sdf::ElementPtr sdf) override;
         void OnUpdate();
+        ~ModelLight();
+        transport::NodePtr node_;
+        transport::SubscriberPtr mast_pose_sub_;
+        transport::SubscriberPtr comm_pose_sub_;
 
-	private:
+    private:
+        void commPoseCallback(ConstPoseStampedPtr &msg);
+        void mastPoseCallback(ConstPoseStampedPtr &msg);
+
         rendering::VisualPtr model_visual_;
         event::ConnectionPtr updateConnection;
-        // int count;
+        int size_;
+        std::string *pose_names_ = NULL;
         ignition::math::Color led_color_;
+        Eigen::Vector3d mast_position_;
+        Eigen::Vector3d comm_block_position_;
+        Eigen::Vector3d comm_wrt_mast_position_; // in mast frame
+
+        Eigen::Quaterniond mast_orientation_;
+        Eigen::Quaterniond comm_block_orientation_;
+        Eigen::Quaterniond comm_wrt_mast_orientation_;
 };
 }
--- src/light_plugin.cpp
+++ src/light_plugin.cpp
@@ -3,16 +3,57 @@
 namespace gazebo {
 
 void ModelLight::Load(rendering::VisualPtr visual_, sdf::ElementPtr sdf_) {
-    // count = 1;
     if (sdf_->HasElement("led_color")) led_color_ = sdf_->Get<ignition::math::Color>("led_color");
     else led_color_ = ignition::math::Color(1,0,0,0);
     model_visual_ = visual_;
     std::cout << "Plugin Loaded" << std::endl;
+    node_ = transport::NodePtr(new transport::Node);
+    node_->Init();
+    comm_pose_sub_ = node_->Subscribe("~/communications_module/link/pose/info", &ModelLight::commPoseCallback, this);
+    mast_pose_sub_ = node_->Subscribe("~/table/mast/pose/info", &ModelLight::mastPoseCallback, this);
+    
     updateConnection = event::Events::ConnectPreRender(std::bind(&ModelLight::OnUpdate, this));  
 }
 
 void ModelLight::OnUpdate() {
     model_visual_->SetEmissive(led_color_);
+}
+
+void ModelLight::commPoseCallback(ConstPoseStampedPtr &msg) {
+    // msg->PrintDebugString();
+    // std::cout << std::endl << std::endl;
+    // size_ = msg.get()->pose_size();
+    // if (pose_names_ == NULL) pose_names_ = new std::string [size_];
+    // msg->Ser
+    // gazebo::msgs::Pose;  
+    // std::cout << msg.get()->pose().size() << std::endl;
+    // for (int i=0; i<size_;i++) {
+    //     pose_names_[i] = std::string(msg->pose(i).name()); 
+    // }
+    // std::cout << msg->pose().position().x() << " " << msg->pose().position().y() << " " << msg->pose().position().z() << std::endl;
+    comm_block_position_ = Eigen::Vector3d(msg->pose().position().x(), msg->pose().position().y(), msg->pose().position().z());
+    comm_block_orientation_ = Eigen::Quaterniond(msg->pose().orientation().w(), msg->pose().orientation().x(), msg->pose().orientation().y(), msg->pose().orientation().z());  
+}
+
+void ModelLight::mastPoseCallback(ConstPoseStampedPtr &msg) {
+    // msg->PrintDebugString();
+    // std::cout << std::endl << std::endl;
+    // size_ = msg.get()->pose_size();
+    // if (pose_names_ == NULL) pose_names_ = new std::string [size_];
+    // msg->Ser
+    // gazebo::msgs::Pose;  
+    // std::cout << msg.get()->pose().size() << std::endl;
+    // for (int i=0; i<size_;i++) {
+    //     pose_names_[i] = std::string(msg->pose(i).name()); 
+    // }
+    // std::cout << msg->pose().position().x() << " " << msg->pose().position().y() << " " << msg->pose().position().z() << std::endl;
+    mast_position_ = Eigen::Vector3d(msg->pose().position().x(), msg->pose().position().y(), msg->pose().position().z());
+    mast_orientation_ = Eigen::Quaterniond(msg->pose().orientation().w(), msg->pose().orientation().x(), msg->pose().orientation().y(), msg->pose().orientation().z());  
+}
+
+ModelLight::~ModelLight() {
+    // FILE *fp = fopen("/home/kshitij/names.txt","a");
+    // for (int i=0; i < size_; i++) fprintf(fp,"%s\n",pose_names_[i].c_str());
 }
 
 GZ_REGISTER_VISUAL_PLUGIN(ModelLight)
