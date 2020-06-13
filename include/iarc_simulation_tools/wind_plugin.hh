37500530fd4b13812fbc7728bd5f8b57a6c94dd9
isro01 <ishir@iitk.ac.in>
2020-06-13 20:05:49 +0530

    Include sdf params in world file for wind plugin
    Build successful on the plugin though is not working due to probable
    improper mention of sdf params in the word file
    
    [Ticket: X]

--- include/iarc_simulation_tools/wind_plugin.hh
+++ include/iarc_simulation_tools/wind_plugin.hh
@@ -1,34 +1,3 @@
-// #ifndef GAZEBO_PLUGINS_WINDPLUGIN_HH_
-// #define GAZEBO_PLUGINS_WINDPLUGIN_HH_
-
-// #include "gazebo/common/Plugin.hh"
-// #include "gazebo/physics/physics.hh"
-// #include <ignition/math/Vector3.hh>
-// #include <memory>
-
-// namespace gazebo
-// {
-//     class WindPluginPrivate;
-
-//     class GZ_PLUGIN_VISIBLE WindPlugin: public WorldPlugin
-//     {
-//         public: WindPlugin();
-//         public: virtual void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf);
-
-//         public: ignition::math::Vector3d LinearVel(
-//             const physics::Wind *_wind,
-//             const physics::Entity *_entity
-//         );
-
-//         private: void OnUpdate();
-//         private: std::unique_ptr<WindPluginPrivate> dataPtr;
-//     };
-// }
-
-// //using namespace gazebo
-// #endif
-
-
 #pragma once
 
 #include <string>
