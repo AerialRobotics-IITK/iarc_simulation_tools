d1b8e14bfbe04ab7b9c397d4c6f90ea42884eacd
isro01 <ishir@iitk.ac.in>
2020-06-28 14:21:25 +0530

    Build not passing

---
+++ include/iarc_simulation_tools/variable_load_plugin.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <vector>
+#include <ros/ros.h>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <ignition/math/Vector3.hh>
+#include <gazebo/transport/transport.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <functional>
+#include <random>
+#include <string>
+#include <iarc_simulation_tools/var_load.h>
+#include <glog/logging.h>
+
+#include <gazebo/common/Plugin.hh>
+
+namespace gazebo {
+
+static const std::string kDefaultFrameId = "world";
+static const std::string kDefaultLinkName = "base_link";
+static const std::string kDefaultNamespace = "namespace";
+
+class GazeboVarForcePlugin : public ModelPlugin {
+
+public:
+  GazeboVarForcePlugin()
+      : ModelPlugin(), namespace_(kDefaultNamespace),
+      frame_id_(kDefaultFrameId), 
+      link_name_(kDefaultLinkName) {}
+
+  virtual ~GazeboVarForcePlugin();
+protected:
+  void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+  void OnUpdate(const common::UpdateInfo & /*_info*/);
+  void cb(const boost::shared_ptr<const iarc_simulation_tools::var_load>& msg);
+
+private:
+  // iarc_simulation_tools::var_load& var_load_;
+  event::ConnectionPtr update_connection_;
+  physics::WorldPtr world_;
+  physics::ModelPtr model_;
+  physics::LinkPtr link_;
+  std::string namespace_;
+  std::string frame_id_;
+  std::string link_name_;
+  ignition::math::Vector3d xyz_offset_;
+  ignition::math::Vector3d force_direction_;
+  gazebo::transport::SubscriberPtr params_sub_;
+  gazebo::transport::NodePtr node_handle_;
+
+};
+}
---
+++ src/variable_load_plugin.cpp
@@ -0,0 +1,60 @@
+#include <iarc_simulation_tools/variable_load_plugin.hpp>
+
+#include "ConnectGazeboToRosTopic.pb.h"
+#include "ConnectRosToGazeboTopic.pb.h"
+#include <iostream>
+#include <math.h>
+
+namespace gazebo {
+
+GazeboVarForcePlugin::~GazeboVarForcePlugin() {}
+
+void GazeboVarForcePlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
+    
+    // model_ = _model;
+    // world_ = model_->GetWorld();
+
+    // if (_sdf->HasElement("robotNamespace"))
+    //     namespace_ = _sdf->GetElement("robotNamespace")->Get<std::string>();
+    // else
+    //     gzerr << "[gazebo_wind_plugin] Please specify a robotNamespace.\n";
+
+    node_handle_ = gazebo::transport::NodePtr(new transport::Node());
+    node_handle_->Init();
+
+    // getSdfParam<std::string>(_sdf, "frameId", frame_id_, frame_id_);
+    // getSdfParam<std::string>(_sdf, "linkName", link_name_, link_name_);
+
+    // link_ = model_->GetLink(link_name_);
+    // if (link_ == NULL)
+    //     gzthrow("[gazebo_wind_plugin] Couldn't find specified link \"" << link_name_
+    //                                                                << "\".");
+    
+    params_sub_ = node_handle_->Subscribe("var_force_topic",10, &GazeboVarForcePlugin::cb);
+
+    update_connection_ = event::Events::ConnectWorldUpdateBegin(
+      boost::bind(&GazeboVarForcePlugin::OnUpdate, this, _1));
+
+}
+
+void GazeboVarForcePlugin::cb(const boost::shared_ptr<const iarc_simulation_tools::var_load>& msg){
+    // var_load_ = msg;
+    std::cout << "x : " << msg.x << std::endl;
+    std::cout << "m : " << msg.m << std::endl;
+}
+
+void GazeboVarForcePlugin::OnUpdate(const common::UpdateInfo &_info){
+
+    common::Time now = world_->SimTime();
+
+    // while(true){
+    //     gazebo::common::Time::MSleep(10);
+    // }
+
+
+}
+
+
+GZ_REGISTER_MODEL_PLUGIN(GazeboVarForcePlugin);
+
+}
