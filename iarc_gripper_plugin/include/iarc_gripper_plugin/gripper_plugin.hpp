f5a612e6fff2d03bd7cc0da33bddb416cf17e580
isro01 <ishir@iitk.ac.in>
2020-08-08 00:48:19 +0530

    Build passing and achieve proper functioning
    Add service to command gripper mechanism and plugin applies torque
    to the screw joint in screw-rod. Used aluminium density for the joints and rod
    and carbon fibre ( 1.55g/cc ) for the flat rods

--- iarc_gripper_plugin/include/iarc_gripper_plugin/gripper_plugin.hpp
+++ iarc_gripper_plugin/include/iarc_gripper_plugin/gripper_plugin.hpp
@@ -14,31 +14,29 @@
 namespace gazebo {
 
 class GripperPlugin : public ModelPlugin {
-  public:
-    void Load(physics::ModelPtr parent, sdf::ElementPtr sdf);
-    void onUpdate(const common::UpdateInfo& /*_info*/);
-    // bool serverCallback(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &resp);
-    bool serverCallback(iarc_gripper_plugin::GripperCmd::Request& req, iarc_gripper_plugin::GripperCmd::Response& res);
+public:
+  void Load(physics::ModelPtr parent, sdf::ElementPtr sdf);
+  void onUpdate(const common::UpdateInfo & /*_info*/);
+  bool serverCallback(iarc_gripper_plugin::GripperCmd::Request &req,
+                      iarc_gripper_plugin::GripperCmd::Response &res);
 
-  private:
-    physics::ModelPtr model_;
-    sdf::ElementPtr sdf_;
-    event::ConnectionPtr updateConnection_;
-    physics::LinkPtr link1_;
-    physics::LinkPtr link2_;
+private:
+  physics::ModelPtr model_;
+  sdf::ElementPtr sdf_;
+  event::ConnectionPtr updateConnection_;
+  physics::LinkPtr link_;
 
-    std::string link_name1_;
-    std::string link_name2_;
-    std::string namespace_;
-    ignition::math::Vector3d force_direction_;
-    ignition::math::Vector3d xyz_offset_;
-    ros::NodeHandle* rosnode_;
+  std::string link_name_;
+  std::string namespace_;
+  ignition::math::Vector3d force_direction_;
+  ignition::math::Vector3d xyz_offset_;
+  ros::NodeHandle *rosnode_;
 
-    double force_magnitude_;
-    bool flag_ = true;
+  double force_magnitude_;
+  bool flag_ = true;
 
-    ros::ServiceServer cmd_server_;
-    boost::mutex lock_;
+  ros::ServiceServer cmd_server_;
+  boost::mutex lock_;
 };
 
-}  // namespace gazebo
+} // namespace gazebo
--- iarc_gripper_plugin/src/gripper_plugin.cpp
+++ iarc_gripper_plugin/src/gripper_plugin.cpp
@@ -6,101 +6,78 @@
 namespace gazebo {
 
 void GripperPlugin::Load(physics::ModelPtr parent, sdf::ElementPtr sdf) {
-    model_ = parent;
-    sdf_ = sdf;
+  model_ = parent;
+  sdf_ = sdf;
 
-    gzmsg << "Gripper plugin loaded for " << model_->GetName() << std::endl;
+  gzmsg << "Gripper plugin loaded for " << model_->GetName() << std::endl;
 
-    if (sdf->HasElement("namespace")) {
-        namespace_ = sdf->GetElement("namespace")->Get<std::string>();
-        gzmsg << namespace_ << std::endl;
-    } else {
-        gzerr << "Element <namespace>> is not specified. Aborting.";
-        return;
-    }
-    if (sdf->HasElement("link_name1")) {
-        link_name1_ = sdf->GetElement("link_name1")->Get<std::string>();
-    } else {
-        gzerr << "Element <link_name1> is not specified. Aborting.";
-        return;
-    }
+  if (sdf->HasElement("namespace")) {
+    namespace_ = sdf->GetElement("namespace")->Get<std::string>();
+    gzmsg << namespace_ << std::endl;
+  } else {
+    gzerr << "Element <namespace>> is not specified. Aborting.";
+    return;
+  }
 
-    if (sdf->HasElement("link_name2")) {
-        link_name2_ = sdf->GetElement("link_name2")->Get<std::string>();
-    } else {
-        gzerr << "Element <link_name2> is not specified. Aborting.";
-        return;
-    }
+  if (sdf->HasElement("link_name")) {
+    link_name_ = sdf->GetElement("link_name")->Get<std::string>();
+  } else {
+    gzerr << "Element <link_name> is not specified. Aborting.";
+    return;
+  }
 
-    if (sdf->HasElement("force_magnitude")) {
-        force_magnitude_ = sdf->GetElement("force_magnitude")->Get<double>();
-    } else {
-        force_magnitude_ = 1.0;
-    }
+  if (sdf->HasElement("force_magnitude")) {
+    force_magnitude_ = sdf->GetElement("force_magnitude")->Get<double>();
+  } else {
+    force_magnitude_ = 1.0;
+  }
 
-    link1_ = model_->GetLink(parent->GetName() + "::" + link_name1_);
-    link2_ = model_->GetLink(parent->GetName() + "::" + link_name2_);
+  link_ = model_->GetLink(parent->GetName() + "::" + link_name_);
 
-    force_direction_.Set(0, -1, 0);
-    xyz_offset_.Set(0, 0, 0);
+  force_direction_.Set(0, 0, 0);
+  xyz_offset_.Set(0, 0, 0);
 
-    if (!ros::isInitialized()) {
-        ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
-                         << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package.");
-        return;
-    }
+  if (!ros::isInitialized()) {
+    ROS_FATAL_STREAM(
+        "A ROS node for Gazebo has not been initialized, unable to load "
+        "plugin. "
+        << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the "
+           "gazebo_ros package.");
+    return;
+  }
 
-    rosnode_ = new ros::NodeHandle(namespace_);
+  rosnode_ = new ros::NodeHandle(namespace_);
+  cmd_server_ = rosnode_->advertiseService(
+      "gripper_cmd", &GripperPlugin::serverCallback, this);
 
-    cmd_server_ = rosnode_->advertiseService("gripper_cmd", &GripperPlugin::serverCallback, this);
-    // ros::ServiceClient gripper_cmd_client = rosnode_.serviceClient("gripper_cmd");
-
-    updateConnection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&GripperPlugin::onUpdate, this, _1));
+  updateConnection_ = event::Events::ConnectWorldUpdateBegin(
+      boost::bind(&GripperPlugin::onUpdate, this, _1));
 }
 
-// bool GripperPlugin::serverCallback( std_srvs::Trigger::Request & req, std_srvs::Trigger::Response &resp){
-//     flag_ = !flag_;
-//     if (flag_==true){
-//         force_direction_ = force_direction_*(-1);
-//         std::cout << "Expanding gripper";
-//     }
-//     else{
-//         std::cout << "Contracting gripper";
-//     }
-//     resp.success = true;
-//     resp.message = "Triggered";
-//     return true;
-// }
+bool GripperPlugin::serverCallback(
+    iarc_gripper_plugin::GripperCmd::Request &req,
+    iarc_gripper_plugin::GripperCmd::Response &res) {
+  boost::mutex::scoped_lock scoped_lock(lock_);
 
-bool GripperPlugin::serverCallback(iarc_gripper_plugin::GripperCmd::Request& req, iarc_gripper_plugin::GripperCmd::Response& res) {
-    boost::mutex::scoped_lock scoped_lock(lock_);
-
-    if (req.flag == 1) {
-        force_direction_ = -force_direction_;
-        ROS_INFO_STREAM("Expanding gripper " << force_direction_);
-    } else {
-        ROS_INFO_STREAM("Retracting gripper" << force_direction_);
-    }
-    res.message = "Triggered";
-    return true;
+  if (req.flag == 1) {
+    force_direction_.Set(0, 0, -1);
+    res.message = "Expanding gripper";
+  } else if (req.flag == 2) {
+    force_direction_.Set(0, 0, 0);
+    res.message = "Pausing gripper";
+  } else if (req.flag == 0) {
+    force_direction_.Set(0, 0, 1);
+    res.message = "Retracting gripper";
+  } else {
+    res.message = "Choose from 0 to retract, 1 to expand or 2 to pause";
+  }
+  return true;
 }
 
-void GripperPlugin::onUpdate(const common::UpdateInfo& _info) {
-    // std_srvs::Trigger srv;
-    // srv.request.flag = 1;
-    // if (client.call(srv)){
-    //     if (){
-    //         force_direction_ = force_direction_*(-1);
-    //         std::cout << "Expanding gripper");
-    //     }
-    //     else{
-    //         std::cout <<"Contracting gripper");
-    //     }
-    // }
+void GripperPlugin::onUpdate(const common::UpdateInfo &_info) {
 
-    link1_->AddForceAtRelativePosition(force_magnitude_ * force_direction_, xyz_offset_);
-    link1_->AddForceAtRelativePosition(force_magnitude_ * force_direction_, xyz_offset_);
+  link_->AddRelativeTorque(force_direction_ * force_magnitude_);
 }
 
 GZ_REGISTER_MODEL_PLUGIN(GripperPlugin);
-}  // namespace gazebo
+} // namespace gazebo
