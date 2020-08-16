748465cae900596972c6b1dac831e2a88ad22ba2
isro01 <ishir@iitk.ac.in>
2020-08-16 23:32:27 +0530

    Build successful but undefined symbol error

---
+++ iarc_wind_plugin/include/iarc_wind_plugin/wind_plugin.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/common/common.hh>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <ros/ros.h>
+#include <string>
+#include <iarc_sim_test_tools/common.h>
+#include <thread>
+
+namespace gazebo{
+
+static const std::string kDefualFrameId = "world";
+static const std::string kDefaultLinkName = "base_link";
+
+class GazeboWindPlugin : public ModelPlugin {
+    public:
+      GazeboWindPlugin()
+        : ModelPlugin()
+        , namespace_(kDefaultNamespace)
+        , link_name_(kDefaultLinkName) {
+        
+      }
+
+      virtual ~GazeboWindPlugin();
+
+    protected:
+        void Load(physics::ModelPtr model, sdf::ElementPtr sdf);
+        void onUpdate(const common::UpdateInfo& _info);
+
+    private:
+    event::ConnectionPtr update_connection_;
+    
+    physics::ModelPtr model_;
+    sdf::ElementPtr sdf_;
+    physics::LinkPtr link_;
+    
+    std::string namespace_;
+    std::string link_name_;
+    
+    ignition::math::Vector3d xyz_offset_;
+    ignition::math::Vector3d force_direction_;
+
+    double windspeed_;
+    double wind_angle_;
+
+};
+}
---
+++ iarc_wind_plugin/src/wind_plugin.cpp
@@ -0,0 +1,57 @@
+#include <iarc_wind_plugin/wind_plugin.hpp>
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+namespace gazebo {
+
+void GazeboWindPlugin::Load(physics::ModelPtr parent, sdf::ElementPtr sdf) {
+  model_ = parent;
+  sdf_ = sdf;
+
+  gzmsg << "Wind plugin loaded for " << model_->GetName() << std::endl;
+
+  if (sdf->HasElement("namespace")) {
+    namespace_ = sdf->GetElement("namespace")->Get<std::string>();
+    gzmsg << namespace_ << std::endl;
+  } else {
+    gzerr << "Element <namespace>> is not specified. Aborting.";
+    return;
+  }
+
+  if (sdf->HasElement("link_name")) {
+    link_name_ = sdf->GetElement("link_name")->Get<std::string>();
+  } else {
+    gzerr << "Element <link_name> is not specified. Aborting.";
+    return;
+  }
+
+  if (sdf->HasElement("windspeed")) {
+    windspeed_ = sdf->GetElement("windspeed")->Get<double>();
+  } else {
+    windspeed_ = 1.0;
+  }
+
+  if (sdf->HasElement("windangle")) {
+    wind_angle_ = sdf->GetElement("windangle")->Get<double>();
+  } else {
+      wind_angle_ = 0;
+    gzerr << "Element <link_name> is not specified. Aborting.";
+    return;
+  }
+
+  link_ = model_->GetLink( link_name_ );
+
+  force_direction_.Set(cos(wind_angle_), sin(wind_angle_), 0);
+
+  update_connection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&GazeboWindPlugin::onUpdate, this, _1));
+
+}
+
+void GazeboWindPlugin::onUpdate(const common::UpdateInfo& _info){
+    link_->AddForce(windspeed_ * force_direction_);
+}
+
+GZ_REGISTER_MODEL_PLUGIN(GazeboWindPlugin);
+
+} //namespace gazbeo
