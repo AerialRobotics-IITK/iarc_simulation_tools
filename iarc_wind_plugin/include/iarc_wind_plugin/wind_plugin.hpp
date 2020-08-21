06e5e90ad7d6c3cb836f2195ccb4fcbc377930ca
Ashwin Shenai <ashwins@iitk.ac.in>
2020-08-22 01:14:45 +0530

    Implement wind dynamics hash table
    Define hash table
    Function for initializing hash table
    Test function in Load
    Custom hasher for wind params, comparison operator
    Default constructors for quick usage

--- iarc_wind_plugin/include/iarc_wind_plugin/wind_plugin.hpp
+++ iarc_wind_plugin/include/iarc_wind_plugin/wind_plugin.hpp
@@ -1,65 +1,94 @@
 #pragma once
 
+#include <bits/stdc++.h>
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/common/common.hh>
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
+#include <iarc_sim_test_tools/common.h>
 #include <ros/ros.h>
 #include <string>
-#include <iarc_sim_test_tools/common.h>
 #include <thread>
-#include <bits/stdc++.h>
 #include <unordered_map>
 
-namespace gazebo{
+namespace gazebo {
 
 static const std::string kDefualFrameId = "world";
 static const std::string kDefaultLinkName = "base_link";
 
 class CustomWindPlugin : public ModelPlugin {
-    public:
-      CustomWindPlugin()
+  public:
+    CustomWindPlugin()
         : ModelPlugin()
         , namespace_(kDefaultNamespace)
         , link_name_(kDefaultLinkName) {
-        
-      }
-      struct wind_params_{
+    }
+
+    typedef struct WindParams {
         double angle;
         double speed;
 
-      };
-      struct dynamics_{
+        WindParams(const double& angle, const double& speed)
+            : angle(angle)
+            , speed(speed){};
+
+        // comparison operator to check hash collisions
+        bool operator==(const WindParams& wind_params) const {
+            return (angle == wind_params.angle) && (speed == wind_params.speed);
+        }
+    } WindParams;
+
+    typedef struct DynParams {
         ignition::math::Vector3d force;
-        // ignition::math::Vector3d torque;
-      };
+        ignition::math::Vector3d torque;
 
-      virtual ~CustomWindPlugin(){};
+        DynParams(const ignition::math::Vector3d& force, const ignition::math::Vector3d& torque)
+            : force(force)
+            , torque(torque){};
 
-    protected:
-        void Load(physics::ModelPtr model, sdf::ElementPtr sdf);
-        void onUpdate(const common::UpdateInfo& _info);
-        void bilinear_interpolation(wind_params_ params);
+        DynParams() {
+            force = ignition::math::Vector3d::Zero;
+            torque = ignition::math::Vector3d::Zero;
+        };
+    } DynParams;
 
-    private:
+    virtual ~CustomWindPlugin(){};
+
+  protected:
+    void Load(physics::ModelPtr model, sdf::ElementPtr sdf);
+    void onUpdate(const common::UpdateInfo& _info);
+    ignition::math::Vector3d interpolateWindDynamics(WindParams params);
+
+  private:
+    // hashing function for wind parameters
+    typedef struct WindHasher {
+        std::size_t operator()(const WindParams& params) const {
+            // Compute individual hash values for angle & speed then combine
+            // http://stackoverflow.com/a/1646913/126995
+            std::size_t hash = 1009;
+            hash = hash * 9176 + std::hash<double>()(params.angle);
+            hash = hash * 9176 + std::hash<double>()(params.speed);
+            return hash;
+        }
+    } WindHasher;
+
+    void initializeFieldTable();
+
     event::ConnectionPtr update_connection_;
-    
+
     physics::ModelPtr model_;
     sdf::ElementPtr sdf_;
     physics::LinkPtr link_;
-    
+
     std::string namespace_;
     std::string link_name_;
-    
+
     ignition::math::Vector3d xyz_offset_;
     ignition::math::Vector3d force_direction_;
 
     double windspeed_;
     double wind_angle_;
 
-    ignition::math::Vector3d interp_force_;
-
-    // std::unordered_map<wind_params_, dynamics_> field_table;
-
+    std::unordered_map<WindParams, DynParams, WindHasher> field_table_;
 };
-}
+}  // namespace gazebo
--- iarc_wind_plugin/src/wind_plugin.cpp
+++ iarc_wind_plugin/src/wind_plugin.cpp
@@ -8,77 +8,87 @@
 namespace gazebo {
 
 void CustomWindPlugin::Load(physics::ModelPtr parent, sdf::ElementPtr sdf) {
-  model_ = parent;
-  sdf_ = sdf;
+    model_ = parent;
+    sdf_ = sdf;
 
-  gzmsg << "Wind plugin loaded for " << model_->GetName() << std::endl;
+    gzmsg << "Wind plugin loaded for " << model_->GetName() << std::endl;
 
-  if (sdf->HasElement("namespace")) {
-    namespace_ = sdf->GetElement("namespace")->Get<std::string>();
-    gzmsg << namespace_ << std::endl;
-  } else {
-    gzerr << "Element <namespace>> is not specified. Aborting.";
-    return;
-  }
+    if (sdf->HasElement("namespace")) {
+        namespace_ = sdf->GetElement("namespace")->Get<std::string>();
+        gzmsg << namespace_ << std::endl;
+    } else {
+        gzerr << "Element <namespace> is not specified. Aborting.";
+        return;
+    }
 
-  if (sdf->HasElement("link_name")) {
-    link_name_ = sdf->GetElement("link_name")->Get<std::string>();
-  } else {
-    gzerr << "Element <link_name> is not specified. Aborting.";
-    return;
-  }
+    if (sdf->HasElement("link_name")) {
+        link_name_ = sdf->GetElement("link_name")->Get<std::string>();
+    } else {
+        gzerr << "Element <link_name> is not specified. Aborting.";
+        return;
+    }
 
-  if (sdf->HasElement("windspeed")) {
-    windspeed_ = sdf->GetElement("windspeed")->Get<double>();
-  } else {
-    windspeed_ = 1.0;
-  }
+    if (sdf->HasElement("windspeed")) {
+        windspeed_ = sdf->GetElement("windspeed")->Get<double>();
+    } else {
+        windspeed_ = 1.0;
+    }
 
-  if (sdf->HasElement("windangle")) {
-    wind_angle_ = sdf->GetElement("windangle")->Get<double>();
-  } else {
-    wind_angle_ = 0;
-  }
+    if (sdf->HasElement("windangle")) {
+        wind_angle_ = sdf->GetElement("windangle")->Get<double>();
+    } else {
+        wind_angle_ = 0;
+    }
 
-  link_ = model_->GetLink(link_name_);
+    link_ = model_->GetLink(link_name_);
 
-  // Input values from cfd analysis
-  // field_calc[] = __
+    // Input values from cfd analysis
+    initializeFieldTable();
 
-  struct wind_params_ params; // Input angle and speed
-  params.angle = 0;
-  params.speed = 5;
+    WindParams test_params(1, 2);  // Input angle and speed
+    gzmsg << interpolateWindDynamics(test_params) << "\n";
 
-  CustomWindPlugin::bilinear_interpolation(params);
-
-  // force_direction_.Set(cos(wind_angle_), sin(wind_angle_), 0);
-  update_connection_ = event::Events::ConnectWorldUpdateBegin(
-      boost::bind(&CustomWindPlugin::onUpdate, this, _1));
+    // force_direction_.Set(cos(wind_angle_), sin(wind_angle_), 0);
+    update_connection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&CustomWindPlugin::onUpdate, this, _1));
 }
 
-void CustomWindPlugin::bilinear_interpolation(wind_params_ params) {
-  // wind_params_ ceil = params.getUpperBounds();
-  // wind_params_ floor = params.getLowerBounds();
+ignition::math::Vector3d CustomWindPlugin::interpolateWindDynamics(WindParams params) {
+    ignition::math::Vector3d interp_force;
 
-  // struct dynamics_ bot_left = field_table[wind_params_(floor.angle,
-  // floor.speed)]; struct dynamics_ bot_right =
-  // field_table[wind_params_(ceil.angle, floor.speed)]; struct dynamics_
-  // top_left = field_table[wind_params_(floor.angle, ceil.speed)]; struct
-  // dynamics_ top_right = field_table[wind_params_(ceil.angle, ceil.speed)];
+    WindParams ceil = params;  // impl getUpperBounds
+    ceil.angle++;
+    ceil.speed++;
+    WindParams floor = params;  // impl getLowerBounds
+    floor.angle--;
+    floor.speed--;
 
-  interp_force =
-      (bot_left * (ceil.angle - params.angle) * (ceil.speed - params.speed) +
-       bot_right * (params.angle - floor.angle) * (ceil.speed - params.speed) +
-       top_left * (ceil.angle - params.angle) * (params.speed - floor.speed) +
-       top_right * (params.angle - floor.angle) * (params.speed - floor.speed));
-  interp_force /= (ceil.angle - floor.angle) * (ceil.speed - floor.speed);
+    CustomWindPlugin::DynParams bot_left = field_table_[CustomWindPlugin::WindParams(floor.angle, floor.speed)];
+    CustomWindPlugin::DynParams bot_right = field_table_[CustomWindPlugin::WindParams(ceil.angle, floor.speed)];
+    CustomWindPlugin::DynParams top_left = field_table_[CustomWindPlugin::WindParams(floor.angle, ceil.speed)];
+    CustomWindPlugin::DynParams top_right = field_table_[CustomWindPlugin::WindParams(ceil.angle, ceil.speed)];
+
+    /*
+        interp_force =
+            (bot_left * (ceil.angle - params.angle) * (ceil.speed - params.speed) + bot_right * (params.angle - floor.angle) * (ceil.speed - params.speed) +
+                top_left * (ceil.angle - params.angle) * (params.speed - floor.speed) + top_right * (params.angle - floor.angle) * (params.speed -
+       floor.speed)); interp_force /= (ceil.angle - floor.angle) * (ceil.speed - floor.speed);
+    */
+    return interp_force;
 };
 
-void CustomWindPlugin::onUpdate(const common::UpdateInfo &_info) {
-  // link_->AddForce(windspeed_ * force_direction_);
-  // link->AddForce(interp_force);
+void CustomWindPlugin::onUpdate(const common::UpdateInfo& _info) {
+    // link_->AddForce(windspeed_ * force_direction_);
+    // link->AddForce(interp_force);
+}
+
+void CustomWindPlugin::initializeFieldTable() {
+    // dummy initialization for testing
+    for (int i = 0; i < 10; i++) {
+        field_table_[WindParams(i, i + 1)] =
+            CustomWindPlugin::DynParams(ignition::math::Vector3d(i + 1, i + 1, i + 1), ignition::math::Vector3d(i + 2, i + 2, i + 2));
+    }
 }
 
 GZ_REGISTER_MODEL_PLUGIN(CustomWindPlugin);
 
-} // namespace gazebo
+}  // namespace gazebo
