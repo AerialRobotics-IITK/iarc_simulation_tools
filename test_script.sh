cf7272bd9df89f3c13eb891b6d00047ad298c828
isro01 <ishir@iitk.ac.in>
2020-09-08 23:50:40 +0530

    Add specific build capacity

--- test_script.sh
+++ test_script.sh
@@ -18,36 +18,36 @@ done
 
 declare -a pkgs
 
-# for f in *; do
-#   if [ -d $f ]
-#   then
-#     # echo "$f"
-#     cd "$f"
-#     # ls
-#     dir="src/"
-#     if [ -d "$dir" ]
-#     then
-#         # echo "$f"
-#         cd src
-#         for cf in *; do
-#             # echo "The file $cf has the following corrections"
-#             cpplint --filter=-legal/copyright,-readability/multiline_comment,-readability/braces,-build/include_order,-build/c++11,-build/include_what_you_use,-runtime/string,-whitespace/indent,-whitespace/comments,+build/namespace,+readability/constructors --linelength=160 $cf
-#         done
-#         cd ..
-#     fi
+for f in *; do
+  if [ -d $f ]
+  then
+    # echo "$f"
+    cd "$f"
+    # ls
+    dir="src/"
+    if [ -d "$dir" ]
+    then
+        # echo "$f"
+        cd src
+        for cf in *; do
+            # echo "The file $cf has the following corrections"
+            cpplint --filter=-legal/copyright,-readability/multiline_comment,-readability/braces,-build/include_order,-build/c++11,-build/include_what_you_use,-runtime/string,-whitespace/indent,-whitespace/comments,+build/namespace,+readability/constructors --linelength=160 $cf
+        done
+        cd ..
+    fi
 
-#     dir2="include/"
-#     if [ -d "$dir2" ]
-#     then
-#         cd include
-#         cd "$f"
-#         for hf in *; do
-#             cpplint --filter=-legal/copyright,-readability/multiline_comment,-readability/braces,-build/include_order,-build/c++11,-build/include_what_you_use,-runtime/string,-whitespace/indent,-whitespace/comments,+build/namespace,+readability/constructors --linelength=160 $hf
-#         done
-#         cd ..
-#         cd ..
-#     fi      
+    dir2="include/"
+    if [ -d "$dir2" ]
+    then
+        cd include
+        cd "$f"
+        for hf in *; do
+            cpplint --filter=-legal/copyright,-readability/multiline_comment,-readability/braces,-build/include_order,-build/c++11,-build/include_what_you_use,-runtime/string,-whitespace/indent,-whitespace/comments,+build/namespace,+readability/constructors --linelength=160 $hf
+        done
+        cd ..
+        cd ..
+    fi      
 
-#     cd ..
-#   fi
-# done
+    cd ..
+  fi
+done
