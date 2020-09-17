7fdc21b259b9bffa64f3e2e6bf7ee565b38bfe7d
isro01 <ishir@iitk.ac.in>
2020-09-17 10:12:00 +0530

    modify script

--- test_script.sh
+++ test_script.sh
@@ -5,7 +5,7 @@ var=($(git diff --name-only HEAD~10))
 
 len=${#var[@]}
 str="a"
-echo "$str"
+# echo "$str"
 for((i=0;i<len;i++)); do
     # cpplint --filter=-legal/copyright,-readability/multiline_comment,-readability/braces,-build/include_order,-build/c++11,-build/include_what_you_use,-runtime/string,-whitespace/indent,-whitespace/comments,+build/namespace,+readability/constructors --linelength=160 ${var[$i]}
 
